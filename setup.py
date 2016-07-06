0790caddc05a931ecdca251b54d1994948460cb1
paramono <alex@paramono.com>
2016-07-06 14:09:40 +0300

    Add powerpoint templates

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='feincms_mediaicons',
-    version='1.0.1',
+    version='1.0.2',
     packages=['feincms_mediaicons'],
     include_package_data=True,
-    license='BSD License',  
+    license='BSD License',
     description='Some templates and static files for media file rendering',
     long_description=README,
     url='https://github.com/paramono/feincms_mediaicons',
@@ -24,7 +24,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
